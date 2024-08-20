1f13b55a2f4fe6e9f9a76bad909dd6ca3988cf01
jpetterson88 <jpmaverick.rj@gmail.com>
2024-08-19 22:10:48 -0300

    updates

--- sistema-smart-tv/src/Usuario.java
+++ sistema-smart-tv/src/Usuario.java
@@ -1,6 +1,5 @@
 public class Usuario {
     public static void main(String[] args) throws Exception {
-        
         SmartTv smartTv = new SmartTv();
 
         System.out.println("TV Ligada? " + smartTv.ligada);
